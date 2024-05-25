6c0a79a80fe6e35911cc87a0932d1a864b6048eb
Ellie Solhjou <elliesolhjou@gmail.com>
2024-05-25 13:34:42 -0500

    Module 1 Assignment review Q2

--- Assignment1.py
+++ Assignment1.py
@@ -63,3 +63,45 @@ for i in range(len(nums)):
     for j in range(i+1, len(nums)):
         print((nums[i], nums[j]))
 
+'''
+2. (20 points) Write code to populate an array with the size n with numbers from 0 to n-1. 
+Afterwards, shuffle (randomly reorder or rearrange)  the numbers in the array.
+Do not use any built-in functions such as shuffle(). However, feel free to use the 
+built-in random() function.
+
+Examples:
+
+[0, 1, 2, 3] -> shuffled: [2, 0, 3, 1]
+[0, 1, 2, 3, 4, 5] -> shuffled: [5, 2, 4, 1, 3, 0]
+And finally, provide the Big O notation for both the average and worst case 
+time complexities of your code.
+'''
+import random
+
+def createArray(n):
+    orderedArray = list(range(n))
+    # swap index
+    randIndex = random.randint(0, n-1)
+    for i in range(n):
+        # swap
+        orderedArray[i], orderedArray[randIndex] = orderedArray[randIndex], orderedArray[i]
+        
+    return orderedArray
+print(createArray(6))
+
+
+
+
+
+
+
+    # print (randNum(5))
+    # for i in range (n):
+    #     if randNum in orgArray:
+    #         randNum(n)
+            
+    #     # orgArray[randNum(n)] = i
+
+    # return orgArray
+
+# print(createArray(10))
