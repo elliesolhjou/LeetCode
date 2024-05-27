f9fd9cdbc6f909d053769a9392b68244a654110b
Ellie Solhjou <elliesolhjou@gmail.com>
2024-05-26 21:57:23 -0500

    Assignment3 complete

---
+++ assignment3.py
@@ -0,0 +1,190 @@
+'''
+
+Describe the worst case data and the best case data for each of the following sorting algorithms.
+Also, include the big O notation for each case.
+Bubble Sort
+
+Selection Sort
+Insertion Sort
+Merge Sort
+Quicksort
+'''
+
+
+'''Implement an insertion sort function.'''
+
+def insertionSort (arr):
+    for i in range(len(arr)):
+        j = i-1
+        while j>=0 and arr[j+1]<arr[j]:
+            arr[j+1], arr[j] = arr[j], arr[j+1]
+            # tmp = arr[j]
+            # arr[j] = arr[j+1]
+            # arr[j+1] = tmp
+            j-=1
+    return arr
+
+print(insertionSort([2,4,62,1,5,7,12]))
+
+'''
+Write a function that accepts an array and 
+returns an array of numbers missing from it
+The input array:
+has a size n
+contains random numbers ranging from 0 to n-1
+may contain duplicate values
+not necessarily sorted
+The output array:
+contains missing numbers ranging from 0 to n-1.
+This function must have a time complexity of O(n) to get full credit.
+
+For example:
+Given the array [0, 3, 6, 7, 3, 3, 0, 4], this function should return [ 1, 2, 5 ]
+
+'''
+def missedNum(arr):
+    # O(n^2)
+    missed=[]
+    n = len(arr)
+    arr.sort()          
+    for i in range(1,n):
+        diff = 0
+        if arr[i]-arr[i-1]>1:
+            diff = arr[i]-arr[i-1]
+            for j in range(1,diff):
+                missed.append(arr[i]-j)
+    return missed
+
+# O(n)
+def missedNumber(arr):
+    n= len(arr)
+    missed=[]
+    max_size= max(arr)
+    presence = [False] * (max_size+1)
+    for num in arr:
+        print(f"number is {num}")
+        # print (num)
+        presence[num] = True
+    for i in range(len(presence)):
+        if presence[i] == False: 
+            missed.append(i)
+    return missed
+
+# arr = [1,2,3,8,3,7,0]
+arr = [0, 3, 6, 7, 3, 3, 0, 4]
+# arr.sort()
+# print(missedNum(arr))
+print(missedNumber(arr))
+
+''''
+4. Write a function that returns the first 
+non-repeating character in a string with O(n) efficiency. 
+It should return none or null if there are no non-repeating 
+conesutive characters.
+
+For example:
+
+string "aaaaabbbbbbc", this function should return "c"
+string "aabab" should return "b"  
+string "aababb" should return  None ("b" is repeating)
+'''
+
+def repFunc(string):
+    # define a set to see if it was presented and try to keep elements of it unique
+    # if a char is repeated consecutively (check with repeat char set)-> we remove it from hash set
+    hset = set()
+    # define a set to add consecutives number
+    repeat_char = set()
+
+    # prev pointer to check with for consecutiveness
+    prev = None
+
+    for ch in string:
+        # if character was never seen before and not repeated -> it is present and unique
+        if ch not in hset and ch not in repeat_char:
+            # add char to the presented char sets
+            hset.add(ch)
+        
+        # if char was presented before and it is consecutive
+        # add to repeatchar_set -> we don't like it so we remove
+        elif ch in hset and ch==prev:
+            hset.remove(ch)
+            repeat_char.add(ch)
+        prev = ch
+    
+    for c in string:
+        if c in hset:
+            return c
+
+    return None
+
+
+
+
+
+
+# print(repFunc("aababb"))
+
+
+
+
+def repFinderChar(str):
+    # for unique chars
+    unique_char= set()
+    # for consec chars
+    repeated_char= set()
+    prev = None
+    for c in str:
+        if c not in unique_char and c not in repeated_char:
+            unique_char.add(c)
+        elif c in unique_char and c== prev:
+            repeated_char.add(c)
+            unique_char.remove(c)
+        prev = c
+    # print(unique_char)
+    # print(repeated_char)
+    for c in str:
+        if c in unique_char:
+            return c
+
+print(repFinderChar("aaaaabbbbbbcc"))
+
+
+'''
+5. Write a function that given an array of integers and a 
+target value, returns the length of the longest subarray with a sum equal 
+to the target value. Write the function with O(n) efficiency for full credit.
+
+Note: while the sliding window technique is acceptable as a solution, 
+try solving this using a hash table.
+
+For example:
+Given an array [3, 1,-1, 2, -1, 5, -2, 3] and a target value of 3,
+the longest subarray length is 5 ([-1, 2, -1, 5, -2])
+'''
+
+
+def largeSum(arr, target):
+    # allPossibleSubArray=[]
+    max_length=0
+    for  i in range(len(arr)): 
+        cur_sum = arr[i]
+        for j in range(i+1, len(arr)):
+            cur_sum+=arr[j]
+            if cur_sum == target:
+                max_length = max(max_length, j-i+1)
+    
+    return max_length
+
+print(largeSum([3, 1, -1, 2, -1, 5, -2, 3], 4))
+            
+
+                
+
+
+    
+
+    # max(len(subarray))
+    # sum(subarray) = target
+    # return len(subarray)
+
--- insertionSort.py
+++ insertionSort.py
@@ -10,6 +10,7 @@ def insertionSort(arr):
             arr[j+1] = arr[j]
             arr[j] = tmp
             j-=1
+    return arr
 
 # recursive binary sort
 # o(nlogn)
@@ -64,3 +65,102 @@ def merge(arr, s, m, e):
             arr[k] = R[j]
             k+=1
             i+=1
+
+# advantage of quick sort over merge sort -> happens in place less memory complexity
+
+# Quick sort O(n^2)
+# pivotal sort -> pick a value and compare other element to pivot
+# height of tree is n in worstcase (ordered array) 
+
+#  1. take a pivot (usually last element)
+#  2. define two pointers
+    #  one to help us iterate through array
+    #  onr to show the position the order element should be placed
+#  3. partiotning (smaller than pivot on left), after iteration pointer reaches pivot element
+#  4. swap position pointer value with pivot value 
+#  5. make recursive calls on left and right side 
+
+def quickSort(arr, s, e):
+    # define base case
+    if e-s+1<=1:
+        return arr
+    
+    # define pivot
+    # assign a value
+    pivot = arr[e]
+
+    # define pointers
+    positionPointer = s
+    # i as second pointer for iteration
+
+    # partitioning: smaller than  pivot move to positionPointer
+    # don't count pivot in iteration - range takes care of it
+    for i in range(s, e):
+        # swap
+        if arr[i]<= pivot:
+            # tmp = arr[positionPointer]
+            # arr[positionPointer] = arr[i]
+            # arr[i] = tmp
+            arr[positionPointer], arr[i] = arr[i], arr[positionPointer]
+            positionPointer +=1
+
+    '''  not updating pivot -pivot is a value an integer not a pointer to the actual memory cell 
+    This line does not correctly place the pivot into the array because it assigns 
+    the value of pivot to the variable pivot, but it does not update the array.
+    You should directly swap the pivot element with the element at positionPointer in the array.
+    arr[positionPointer] , pivot = pivot , arr[positionPointer]
+    '''
+    # Swap/Move last element (AKA pivot) to correct location
+    arr[positionPointer], arr[e] = arr[e], arr[positionPointer]
+
+    # recursive call on left side (smaller than pivot)
+    # position pointer is at right place
+    quickSort(arr, s, positionPointer-1)
+
+    # recursive call on left side (smaller than pivot)
+    quickSort(arr, positionPointer+1, e)
+
+    return arr
+    # since it is all happening in place no need to merge
+
+# unordered_array = [10, 7, 8, 9, 1, 5, 3, 6, 2, 4]
+unordered_array = [1,3,59,8,3,6,2,7,9]
+fc = quickSort(unordered_array, 0, len(unordered_array)-1)
+print(fc)
+
+
+
+# selection sort - O(n ^ 2)
+# find minimum element and swap with i element where we are at ith iteration
+def selectionSort (arr):
+    # define two pointers
+        # one pointer to iterate (i)
+        # one pointer to find minimum val between arr[i] and rest of array (arr-arr[:i])
+    for i in range(len(arr)):
+        # Find the minimum element in the remaining unsorted array
+        min_index = i
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[min_index]:
+                # before index J is sorted so move pointer
+                # 
+                min_index = j
+        # when found min val index at each subarray iteration then swap it with arr[i]
+        # Swap the found minimum element with the first element of the unsorted part
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
+
+
+
+# Bubble Sort
+# in iteration when first no swap happens, pointer moves to next element
+def bubble_sort(arr):
+    n = len(arr)
+    # Traverse through all array elements
+    for i in range(n):
+        # Last i elements are already in place
+        for j in range(0, n-i-1):
+            # Traverse the array from 0 to n-i-1
+            # Swap if the element found is greater than the next element
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
