39fa05c3b147989d2f33654085983114e99d7293
Ellie Solhjou <elliesolhjou@gmail.com>
2024-05-28 18:50:56 -0500

    HEAP IMPLEMENTATION ADN MORE

--- 0 - HeapImplementation.py
+++ 0 - HeapImplementation.py
@@ -12,6 +12,12 @@
         - no hole in tree -> structure property
         - order property -> defines min and max heap
 '''
+'''
+    leftChild = 2*i
+    rightChild = (2*i)+1
+    parent = i//2
+
+'''
 from dsa import heap
 class Heap:
     def __init__(self):
@@ -96,3 +102,49 @@ class Heap:
             else:
                 break
         return res
+    
+# ========================================================================================================#
+#                                      HEAPIFY - O(N)
+# ========================================================================================================#
+'''
+Recall that to build a binary search tree with n elements, the time complexity is O(n log n). If we build our heap of size 
+n by pushing each element this would also run in O(n log n) time. But there is actually a more efficient algorithm known as Heapify, which allows us to perform this operation in 
+O(n) time.
+'''
+'''
+    leftChild = 2*i
+    rightChild = (2*i)+1
+    parent = i//2
+
+'''
+class Heap:
+    def __init__(self):
+        self.heap = [0]
+        
+    def heapify(self, arr):
+        # 0-th position is moved to the end
+        arr.append(arr[0])
+
+        self.heap = arr
+        cur = (len(self.heap) - 1) // 2
+        while cur > 0:
+            # Percolate down
+            i = cur
+            while 2 * i < len(self.heap):
+                if (2 * i + 1 < len(self.heap) and 
+                self.heap[2 * i + 1] < self.heap[2 * i] and 
+                self.heap[i] > self.heap[2 * i + 1]):
+                    # Swap right child
+                    tmp = self.heap[i]
+                    self.heap[i] = self.heap[2 * i + 1]
+                    self.heap[2 * i + 1] = tmp
+                    i = 2 * i + 1
+                elif self.heap[i] > self.heap[2 * i]:
+                    # Swap left child
+                    tmp = self.heap[i]
+                    self.heap[i] = self.heap[2 * i]
+                    self.heap[2 * i] = tmp
+                    i = 2 * i
+                else:
+                    break
+            cur -= 1
