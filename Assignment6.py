5ef3f935f24f054c6308027456b7d33e2e544cb5
Ellie Solhjou <elliesolhjou@gmail.com>
2024-05-28 17:47:18 -0500

    Graph Implementation Addedd

---
+++ Assignment6.py
@@ -0,0 +1,87 @@
+'''
+1. Write a depth first search function that accepts
+a starting vertex and an ending vertex. 
+If the ending vertex is found, It should also 
+return the path of the search.
+'''
+from collections import deque
+def dfs(start, target, adjList, visit = None, path = None):
+    if visit is None:
+        visit = set()
+    if path is None:
+        path = []
+    if start == target:
+        # path.append(start)
+        return path
+    
+    visit.add(start)
+    path.append(start)
+
+    for neighbor in adjList[start]:
+            if neighbor not in visit:
+                result = dfs(neighbor, target, adjList, visit.copy(), path.copy())
+                if result is not None:
+                    return result
+            
+    return None       
+
+adjList = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
+start_vertex = 'A'
+end_vertex = 'F'
+print(dfs(start_vertex, end_vertex, adjList))
+
+
+
+
+'''
+
+2. Implement a breadth first search function that accepts a starting 
+vertex and an ending vertex of a graph. This time, assume that the graph is 
+implemented using an adjacency matrix.
+'''
+
+
+
+
+
+'''
+3. Why does Djikstra's Algorithm NOT work for graphs with negative weights?
+
+4. Write a function that accepts a directed acyclic graph (DAG) and a path. It should return whether the path is valid or not.
+
+ 
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
